18ffb7322ced4717c6bc86fdcee8f716426a6385
Rahul Dey <rahuldey832000@gmail.com>
2023-08-05 21:17:00 +0530

    ADD Search Functioality

--- Frontend/src/screens/Search.tsx
+++ Frontend/src/screens/Search.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  ActivityIndicator,
+  FlatList,
   SafeAreaView,
   StyleSheet,
+  Text,
   TextInput,
   TouchableOpacity,
   View,
@@ -9,8 +12,28 @@ import {
 import {COLORS, FONTS, SIZES} from '../../constant/theme';
 import Feather from 'react-native-vector-icons/Feather';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
+import ProductCardView from '../components/products/ProductCardView';
 
 const Search = () => {
+  const [searchKey, setSearchKey] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `http://192.168.0.164:3000/products/search/${searchKey}`,
+      );
+      setSearchResult(response.data.data);
+      setLoading(false);
+    } catch (error: any) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <SafeAreaView>
       <View style={styles.searchConatiner}>
@@ -24,13 +47,13 @@ const Search = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=""
-            onPressIn={() => {}}
+            value={searchKey}
+            onChangeText={setSearchKey}
             placeholder="What are you looking for"
           />
         </View>
         <View>
-          <TouchableOpacity style={styles.searchBtn}>
+          <TouchableOpacity style={styles.searchBtn} onPress={handleSearch}>
             <Feather
               name="search"
               size={SIZES.large}
@@ -39,6 +62,23 @@ const Search = () => {
           </TouchableOpacity>
         </View>
       </View>
+      <View style={styles.container}>
+        {loading ? (
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size={SIZES.xxLarge} color={COLORS.primary} />
+          </View>
+        ) : searchResult.length > 0 && searchKey.length > 0 ? (
+          <FlatList
+            data={searchResult}
+            numColumns={2}
+            keyExtractor={(item: any) => item._id}
+            renderItem={({item}) => <ProductCardView product={item} />}
+            contentContainerStyle={styles.container}
+          />
+        ) : (
+          <Text>No Item Found</Text>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
@@ -46,6 +86,16 @@ const Search = () => {
 export default Search;
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingTop: SIZES.small / 2,
+    paddingLeft: SIZES.small / 2 - 2,
+  },
+  loadingContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: SIZES.xLarge,
+  },
   searchBtn: {
     width: 50,
     height: '100%',
